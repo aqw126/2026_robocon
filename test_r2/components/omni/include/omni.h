1d2a2c079e8db2c864f9965be216887ceeb71c03
aqw126 <285932941+aqw126@users.noreply.github.com>
2026-08-06 18:06:20 +0900

    仮

---
+++ test_r2/components/omni/include/omni.h
@@ -0,0 +1,10 @@
+#ifndef OMNI_H
+#define OMNI_H
+
+
+void omni_init(void);
+
+void omni_move(float vx, float vy);
+
+
+#endif
--- test_r2/main/main.c
+++ test_r2/main/main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "omni.h"
+
 
 void app_main(void)
 {
+    omni_init();
 
+    while(1)
+    {
+        // 前進
+        omni_move(0.0, 1.0);
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+
+        // 後退
+        omni_move(0.0, -1.0);
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+
+        // 右移動
+        omni_move(1.0, 0.0);
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+
+        // 左移動
+        omni_move(-1.0, 0.0);
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+
+        // 停止
+        omni_move(0,0);
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
 }
